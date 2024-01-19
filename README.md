41ffdd19c5fe9f66569d40c83f1ec1ac8168a1b4
Igor Skripnikov <garik108@narod.ru>
2024-01-19 23:03:50 +0300

    ver 1

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# terraform_yandex
+## ДЗ 14 - terraform yandex template
+
+  - ## convert my terraform OAuth-token to IAM-token
+  - sudo curl -sSL https://storage.yandexcloud.net/yandexcloud-yc/install.sh | bash
+  - yc init
+  - yc config list
+  - ## prepare infrastructure
+  - sudo apt update
+  - sudo apt-get update
+  - sudo apt-get install mc -y
+  - sudo mc
+  - apt install git
+  - apt install unzip
+  - ## install terraform
+  - cd /tmp
+  - git clone https://github.com/spring108/terraform.git
+  - cd /tmp/terraform
+  - unzip terraform_1.7.0_linux_amd64.zip
+  - cp ./terraform /bin
+  - terraform --version
+  - ## load project
+  - cd /home/igor
+  - git clone https://github.com/spring108/terraform_yandex.git
+  - cd /home/igor/terraform_yandex
+  - ## setup my IAM-token into config.tf
+  - terraform init
+  - terraform plan
+  - terraform apply
+
+
---
+++ config.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+
+# provider properties
+provider "yandex" {
+  #my autorization tocken
+  token     = "MY_YANDEX_TOKEN"
+
+  #my yandex cloud identifier
+  cloud_id  = "b1gogrmv0lhpqnt6hqu1"
+
+  #my yandex folder identifier (default)
+  folder_id = "b1g5ks1opqq9pgacsaoo"
+
+  # YANDEX ZONE: ru-central1-a, ru-central1-b, ru-central1-d
+  zone = "ru-central1-a"
+}
+
+
+
+
+#############################################################
+### VM1
+#############################################################
+# vm-1 resource configurations
+resource "yandex_compute_instance" "vm-1" {
+  name = "my_vm1"
+  allow_stopping_for_update = true
+  resources {
+    cores  = 1
+    memory = 2
+  }
+  boot_disk {
+    disk_id = yandex_disk.ubuntu2004-15GB.id
+  }
+  network_interface {
+    subnet_id = "e9buvssk2htbkq921avo"
+    nat       = true
+  }
+  metadata = {
+    user-data = "${file("./users.yml")}" #user public ssh-authorized-keys in file
+  }
+  scheduling_policy {
+    preemptible = true 
+  }
+  # init VM -------------------------
+  connection {
+    type     = "ssh"
+    user     = "user"
+    private_key = file("./private_key.ppk")
+    host = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+  }
+  #provisioner "file" {
+  #  source      = "./local_file"
+  #  destination = "/tmp/remote_file"
+  #}
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update", 
+      "sudo apt-get update", 
+      "sudo apt install mc -y",
+    ]
+  }
+
+}
+
+
+
+#############################################################
+### TEMPLATES
+#############################################################
+# boot disk template = ubuntu 20.04 with 15GB
+resource "yandex_disk" "ubuntu2004-15GB" {
+  type     = "network-hdd"
+  zone     = "ru-central1-a"
+  image_id = data.yandex_disk.ubuntu2004_image.id
+  size = 15
+}
+
+# boot disk template = ubuntu 20.04
+data "yandex_disk" "ubuntu2004_image" {
+  family = "ubuntu-2004-lts"
+}
+
+
+
